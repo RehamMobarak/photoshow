7e04eb677d9c5d3054724590be8a0c2e29d15415
RehamMobarak <reham.a.mobarak@gmail.com>
2021-08-20 19:18:06 +0200

    add: create album form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AlbumsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [AlbumsController::class, 'index']);
+Route::get('/albums', [AlbumsController::class, 'index']);
+Route::get('/albums/create', [AlbumsController::class, 'create']);
+Route::post('/albums/store', [AlbumsController::class, 'store'])->name('album-store');
