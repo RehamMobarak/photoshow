e90e893ff07f0e0295ff64aa31a0e8057caa221e
RehamMobarak <reham.a.mobarak@gmail.com>
2021-08-20 18:33:37 +0200

    photos,albums contrls+migrs+models

---
+++ database/migrations/2021_08_20_130040_create_photos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('album_id');
+            $table->string('PhotoName'); //('photo') on tutorial
+            $table->string('Title');
+            $table->string('Size');
+            $table->string('Description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
