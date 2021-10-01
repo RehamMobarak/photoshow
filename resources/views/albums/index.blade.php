0fdcbfa2e65320d5fa14740dd3b4a5722f7399a5
RehamMobarak <reham.a.mobarak@gmail.com>
2021-10-02 00:17:39 +0200

    fix: apply styles on app #3

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>index</title>
-</head>
-<body>
-    index
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+
+<h1 class="me">index</h1>
+<a href="http://photoshow.stg/albums/create">Create</a>
+@endsection
