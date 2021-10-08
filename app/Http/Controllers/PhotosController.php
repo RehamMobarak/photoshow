8efff61b6ea443cffdea692566107f50a9838215
RehamMobarak <reham.a.mobarak@gmail.com>
2021-10-08 04:39:24 +0200

    add: show() destroy() and show album photos

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Photo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PhotosController extends Controller
 {
@@ -15,7 +16,6 @@ class PhotosController extends Controller
 
     public function store(Request $request)
     {
-        // dd($request);
         $this->validate($request, [
             "title" => 'required',
             "description" => 'required',
@@ -23,7 +23,7 @@ class PhotosController extends Controller
         ]);
 
         // storing cover image name with seconds timestamp
-        $fileNameWithExtention = $request->file('photo')->getClientOriginalName();
+        $fileNameWithExtention = $request->file('photo')->getClientOriginalName(); //full name
         $extention = $request->file('photo')->getClientOriginalExtension();
         $fileName = pathinfo($fileNameWithExtention, PATHINFO_FILENAME);
         $fileNameToStore = $fileName . "_" . time() . "." . $extention;
@@ -41,12 +41,25 @@ class PhotosController extends Controller
         $photo->description = $request->input('description');
         $photo->save();
 
-        return redirect('/')->with('success', 'photo uploaded successfully ! ');
+        return redirect('/albums/'.$request->input('albumId'))->with('success', 'photo uploaded successfully ! ');
     }
 
-    // public function show($id)
-    // {
-    //     $photo = photo::with('photos')->find($id);
-    //     return view('photos.show')->with('photo', $photo);
-    // }
+    public function show($id)
+       {
+        $photo = Photo::find($id);
+        return view('photos.show-photo')->with('photo', $photo);
+    }
+
+    public function destroy($id)
+    {
+     $photo = Photo::find($id);
+
+        if(Storage::delete("/public/albums/".$photo->album_id."/".$photo->PhotoName)){
+            $photo->delete();
+
+            return redirect("/albums/".$photo->album_id)->with("success","photo deleted successfully");
+        }else {
+            return redirect("/albums/".$photo->album_id)->with("failed","Error deleting photo !!");
+        }
+ }
 }
--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -12,4 +12,37 @@
         </div>
     </div>
 </section>
+
+@if (count($album->photos) > 0)
+<div class="container">
+    <div class="row">
+        @foreach ($album->photos as $photo)
+        <div class="col-md-3">
+            <div class="card mb-4 shadow-sm">
+                <img src="/storage/albums/{{$album->id}}/{{$photo->PhotoName}}" alt="{{$photo->PhotoName}}"
+                    height="250px">
+                <div class="card-body">
+                    <p class="card-text">{{$photo->Description}}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group ">
+                            <a href="{{route('photo-show', $photo->id)}}"
+                                class="btn btn-sm btn-outline-secondary ">View</a>
+
+                            <form action="{{route('photo-delete', $photo->id)}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class='btn btn-sm btn-danger '>Delete</button>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+@else
+<h1> No photos yet </h1>
+@endif
 @endsection
