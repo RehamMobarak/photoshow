e18ecd1bfbe2097f4bbf194cf7c91624eda7c725
RehamMobarak <reham.a.mobarak@gmail.com>
2021-10-08 03:03:12 +0200

    modify album thumbnail size

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -7,7 +7,7 @@
         @foreach ($albums as $album)
         <div class="col-md-3">
             <div class="card mb-4 shadow-sm">
-                <img src="/storage/cover_images/{{$album->Cover_image}}" alt="cover-image" width="100%" height="100%">
+                <img src="/storage/cover_images/{{$album->Cover_image}}" alt="cover-image"  height="250px">
                 <div class="card-body">
                     <p class="card-text">{{$album->Description}}</p>
                     <div class="d-flex justify-content-between align-items-center">
